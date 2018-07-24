acb77ddd6737fee8fe0efa5c2bf2518da25b6b87
z1994 <2694942998@qq.com>
2018-07-24 22:12:15 +0800

    a

--- JavaScript.1/js/1.js
+++ JavaScript.1/js/1.js
@@ -1,91 +1,39 @@
-
-// ====================================
-
-
-// var list = document.getElementsByClassName('let_style');
-// var run = document.getElementById('run');
-// var stop = document.getElementById('stop'); 
-// var time;
-
-
-
-// function begin() {
-//     var one = Math.floor(Math.random() * list.length);
-//     var two = Math.floor(Math.random() * list.length);
-//     var three = Math.floor(Math.random() * list.length);
-   
-//     if (one == two) {
-//         one = Math.floor(Math.random() * list.length);
-//     } else if (two == three) {
-//         two = Math.floor(Math.random() * list.length);
-//     } else if (one == three) {
-//         three = Math.floor(Math.random() * list.length);
-//     }
-//     list[one].style.backgroundColor = 'rgb' + colors();
-//     list[two].style.backgroundColor = 'rgb' + colors();
-//     list[three].style.backgroundColor = 'rgb' + colors();
-// //  console.log(one)
-// //     console.log(two)
-// //     console.log(three)
-
-// }
-
-// function colors() {
-//     var rgb;
-//     var r = Math.floor(Math.random() * 265);
-//     var g = Math.floor(Math.random() * 265);
-//     var b = Math.floor(Math.random() * 265);
-//     rgb = '(' + r + ',' + g + ',' + b + ')';
-//     return rgb;
-// }
-// run.onclick = function () {
-//     clearInterval(time);
-//     time = setInterval(function () {
-//         for (var i = 0; i < list.length; i++) {
-//             list[i].style.backgroundColor = '';
-//         }
-//         begin();
-
-//     }, 1000);
-// }
-// stop.onclick = function () {
-//     clearInterval(time);
-//     for (var i = 0; i < list.length; i++) {
-//         list[i].style.backgroundColor = '';
-//     }
-// }
-// 
-
-// var list=document.getElementsByClassName('let_style');
-// var run=document.getElementById('run');
-// var stop=document.getElementById('stop');
-// var time;//??????????????
+// var list=document.getElementsByClassName("let_style");//获取节点
+// var run=document.getElementById("run");
+// var stop=document.getElementById("stop");
+// var time;//???
 
 // function begin(){
 //     var one=Math.floor(Math.random()*list.length);
 //     var two=Math.floor(Math.random()*list.length);
 //     var three=Math.floor(Math.random()*list.length);
+// if(one==two || two==three || three==one) {
+//     one=Math.floor(Math.random()*list.length);
+//     two=Math.floor(Math.random()*list.length);
+//     three=Math.floor(Math.random()*list.length);
 
-//     if (one==two){
-//         one=Math.floor(Math.random()*list.length);
-//     }else if(two==three){
-//         two=Math.floor(Math.random()*list.length);
-//     }else if(three==one){
-//         three=Math.floor(Math.random()*list.length);
-//     }
-//     list[one].style.background='rgb'+colors();
-//     list[two].style.background='rgb'+colors();
-//     list[three].style.background='rgb'+colors();
 // }
+// console.log(one,two,three)
+// list[one].style.background='rgb'+colors();
+// list[two].style.background='rgb'+colors();
+// list[three].style.background='rgb'+colors();
+
+// // console.log(one)
+// //     console.log(two)
+// //     console.log(three)
+// }
+
 
 // function colors(){
 //     var rgb;
 //     var r=Math.floor(Math.random()*265);
 //     var g=Math.floor(Math.random()*265);
 //     var b=Math.floor(Math.random()*265);
+
 //     rgb='('+r+','+g+','+b+')';
 //     return rgb;
 // }
+
 // run.onclick=function(){
 //     clearInterval(time);
 //     time=setInterval(function(){
@@ -93,7 +41,7 @@
 //             list[i].style.backgroundColor='';
 //         }
 //         begin();
-//     },100);
+//     },1000);
 // }
 // stop.onclick=function(){
 //     clearInterval(time);
@@ -101,55 +49,101 @@
 //         list[i].style.backgroundColor='';
 //     }
 // }
+// ----------------------------------------------------------
 
-// ==========
+var list = document.getElementsByClassName("let_style");
+var run = document.getElementById("run");
+var stop = document.getElementById("stop");
+var time;
 
 
-var list=document.getElementsByClassName("let_style");//获取节点
-var run=document.getElementById("run");
-var stop=document.getElementById("stop");
-var time;//???
-
-function begin(){
-    var one=Math.floor(Math.random()*list.length);
-    var two=Math.floor(Math.random()*list.length);
-    var three=Math.floor(Math.random()*list.length);
-if(one==two || two==three || three==one) {
-    one=Math.floor(Math.random()*list.length);
-    two=Math.floor(Math.random()*list.length);
-    three=Math.floor(Math.random()*list.length);
-}
-list[one].style.background='rgb'+colors();
-list[two].style.background='rgb'+colors();
-list[three].style.background='rgb'+colors();
-
-// console.log(one)
-//     console.log(two)
-//     console.log(three)
-}
-  
-
-function colors(){
-    var rgb;
-    var r=Math.floor(Math.random()*265);
-    var g=Math.floor(Math.random()*265);
-    var b=Math.floor(Math.random()*265);
-    rgb='('+r+','+g+','+b+')';
+function rgb() {
+    var r = Math.floor(Math.random() * 265);
+    var g = Math.floor(Math.random() * 265);
+    var b = Math.floor(Math.random() * 265);
+    var rgb = "(" + r + "," + g + "," + b + ")"
     return rgb;
 }
 
-run.onclick=function(){
-    clearInterval(time);
-    time=setInterval(function(){
-        for(var i=0;i<list.length;i++){
-            list[i].style.backgroundColor='';
-        }
-        begin();
-    },1000);
-}
-stop.onclick=function(){
-    clearInterval(time);
-    for(var i=0;i<list.length;i++){
-        list[i].style.backgroundColor='';
+function begin() {
+    var one = Math.floor(Math.random() * list.length);
+    var two = Math.floor(Math.random() * list.length);
+    var three = Math.floor(Math.random() * list.length);
+    if (one != two && two != three && three != one) {
+        list[one].style.background = "rgb" + rgb();
+        list[two].style.background = "rgb" + rgb();
+        list[three].style.background = "rgb" + rgb();
+    } else {
+        //调用自身
+        arguments.callee(); 
+        // begin();
     }
 }
+
+
+
+run.onclick = function () {
+    clearInterval(time);
+    time = setInterval(function () {
+        for (var i = 0; i < list.length; i++) {
+            list[i].style.backgroundColor = '';
+        }
+        begin();
+    }, 1000);
+}
+stop.onclick = function () {
+    clearInterval(time);
+    for (var i = 0; i < list.length; i++) {
+        list[i].style.backgroundColor = '';
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// -------------------------------------------
+// var input = document.getElementById('myinput')
+
+// function change () {
+//     var x = document.getElementById("myinput").value;  
+//     var z = Math.round(x / 2 + x / 6 + x / 24.1);
+//     var y = x - z;
+//     document.getElementById("killer").innerHTML = y;
+//     document.getElementById("civ").innerHTML = z;
+//     if(4 > x || 18 < x){
+//         document.getElementById("killer").innerHTML = "";
+//     document.getElementById("civ").innerHTML = "";
+//     }
+// }
+
+// change()
+
+
+
+// input.oninput = function myf() {
+//     change()
+// }
+
+// var go = document.getElementById('go');//这是点击事件的节点
+
+// // 这是点击事件
+//  go.onclick = function () {
+// var x = document.getElementById("myinput").value; 
+// if (4 > x || 18 < x) {
+//        confirm('人数不对')
+//     }
+//     else {
+//         location.href = "./home.html" }
+
+// }
--- JavaScript2/js/1.js
+++ JavaScript2/js/1.js
@@ -1,7 +1,61 @@
-// var input=document.getElementById("a");
+var input = document.getElementById('myinput')
 
-var input = document.getElementById("input");
-input.onpropertychange = function(){
-//   console.log("s++");
-alert("input")
+function change () {
+    var x = document.getElementById("myinput").value; //总人数 
+    var z = Math.round(x / 2 + x / 6 + x / 24.1);//平民人数
+    var y = x - z;//杀手人数
+    document.getElementById("killer").innerHTML = y;//HTML显示杀手人数
+    document.getElementById("civ").innerHTML = z;//HTML显示平民人数
+    if(4 > x || 18 < x){//人数不正确判断
+        document.getElementById("killer").innerHTML = "";
+    document.getElementById("civ").innerHTML = "";
+    }
+}
+
+change()
+
+input.oninput = function myf() {
+    change()
+}
+
+var go = document.getElementById('go');//这是点击事件的节点
+
+// 这是点击事件
+ go.onclick = function () {
+var x = document.getElementById("myinput").value; 
+if (4 > x || 18 < x) {
+       confirm('人数不对')
+    }
+    else {
+        location.href = "./home.html" }
+}
+
+var back=document.getElementById("back");
+back.onclick=function(){
+    console.log(11)
+    if(confirm("返回页面")){
+        location.href="./home.html";
+    }else{
+        return false;
+    }
+}
+
+
+
+
+
+ //键盘事件，抬起按键触发
+document.onkeyup=function(v){ //a时按键信息对象以函数参数的形式传递进来
+    //取出按键信息中的按键代码（大部分浏览器通过keyCode属性获取按键代码，但少部分浏览器使用的是charCode
+    var x= v.keyCode;
+    if (x===13){//13为回车键的编码
+
+        //按下回车键执行如下代码
+        var y=document.getElementById("myinput").value;
+        if(y<4||y>18){
+            confirm("人数不对")
+        }else{
+            location.href="./home.html";
+        }
+    }   
 }
