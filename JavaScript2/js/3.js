9690a7350b6c94b457f13878c785c1473046731f
z1994 <2694942998@qq.com>
2018-07-31 17:51:36 +0800

    1

---
+++ JavaScript2/js/3.js
@@ -0,0 +1,17 @@
+var order = JSON.parse(localStorage.getItem('key'));
+
+box=document.getElementById("box");
+player = document.getElementById("main_font");
+number = document.getElementById("main_number");
+
+
+var arr = [];
+var arr = order;
+
+function run() {
+    var boxes;
+    boxes = arr.length;
+    console.log(boxes);
+}
+
+run();
